94a36fe2f00efc75b2024ab974cd2c777af85354
hydazz <alexanderhyde@icloud.com>
2026-05-24 15:21:34 +1000

    chore(web): add icon/favicon

--- web/src/components/brand/woodstar-mark.tsx
+++ web/src/components/brand/woodstar-mark.tsx
@@ -1,5 +1,3 @@
-import { Star } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 
 type WoodstarMarkSize = "sm" | "md";
@@ -23,9 +21,21 @@ export function WoodstarMark({ className, size = "sm" }: WoodstarMarkProps) {
   return (
     <div
       aria-hidden="true"
-      className={cn("relative flex shrink-0 items-center justify-center text-primary", sizeClass[size], className)}
+      className={cn("relative flex shrink-0 items-center justify-center", sizeClass[size], className)}
     >
-      <Star className={iconClass[size]} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 1024 1024"
+        className={cn("text-[#225241] dark:text-white", iconClass[size])}
+      >
+        <path d="M512,572c-109-174-88-379,0-572,88,193,109,398,0,572Z" />
+        <path d="M566,586c79-166,238-226,396-267-87,130-213,250-396,267Z" />
+        <path d="M458,586c-79-166-238-227-395-266,88,131,213,249,395,266Z" />
+        <path d="M558,644c180-49,342,37,466,163-181,24-346-13-466-163Z" />
+        <path d="M466,645c-180-49-342,36-466,162,181,25,346-12,466-162Z" />
+        <path d="M512,664c70,124,56,237,0,360-56-123-70-236,0-360Z" />
+      </svg>
     </div>
   );
 }
